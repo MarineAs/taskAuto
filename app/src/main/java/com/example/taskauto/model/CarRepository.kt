20913e312de94a0feeaddc50e8ef350e8e436a37
NGS02 <https://github.com/MarineAs/taskAuto>
2020-02-21 18:18:58 +0400

    async to executor

--- app/src/main/java/com/example/taskauto/model/CarRepository.kt
+++ app/src/main/java/com/example/taskauto/model/CarRepository.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.AsyncTask
 import androidx.lifecycle.LiveData
 import com.example.taskauto.model.room.*
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 class CarRepository(
     context: Context,
@@ -13,6 +15,7 @@ class CarRepository(
     private val carList: LiveData<List<CarEntity>>?
     private val modelDao: ModelDao?
     private val modelList: LiveData<List<ModelEntity>>?
+    private var executors = Executors.newSingleThreadExecutor()
 
     init {
         carDao = db.carDao()
@@ -22,15 +25,18 @@ class CarRepository(
     }
 
     fun insert(data: CarEntity) {
-        InsertCarAsyncTask(carDao).execute(data)
+        executors.submit(InsertCar(carDao, data))
+        executors.shutdown()
     }
 
     fun update(data: CarEntity) {
-        UpdateCarAsyncTask(carDao).execute(data)
+        executors.submit(UpdateCar(carDao, data))
+        executors.shutdown()
     }
 
     fun delete(id: Int) {
-        DeleteCarAsyncTask(carDao).execute(id)
+        executors.submit(DeleteCar(carDao, id))
+        executors.shutdown()
     }
 
     fun getAllCarList(): LiveData<List<CarEntity>>? {
@@ -42,40 +48,39 @@ class CarRepository(
     }
 
     fun getModelsList(manName: String?): ModelEntity? {
-        val data = GetList(modelDao).execute(manName).get()
+       val data= executors.submit(GetModelsList(modelDao,manName)).get()
+        executors.shutdown()
         return data
     }
 
     companion object {
-
         fun getInstance(context: Context): CarRepository {
             return CarRepository(context)
         }
+    }
 
-        class InsertCarAsyncTask(dataDao: CarDao?) : AsyncTask<CarEntity, Unit, Unit>() {
-            private var carDao: CarDao? = dataDao
-            override fun doInBackground(vararg params: CarEntity?) {
-                carDao?.insert(params[0])
-            }
+    class InsertCar(private var carDao: CarDao?, private var data: CarEntity) : Runnable {
+        override fun run() {
+            carDao?.insert(data)
         }
+    }
 
-        class UpdateCarAsyncTask(dataDao: CarDao?) : AsyncTask<CarEntity, Unit, Unit>() {
-            private var carDao: CarDao? = dataDao
-            override fun doInBackground(vararg params: CarEntity?) {
-                carDao?.update(params[0])
-            }
+    class UpdateCar(private var carDao: CarDao?, private var data: CarEntity) : Runnable {
+        override fun run() {
+            carDao?.update(data)
         }
+    }
 
-        class DeleteCarAsyncTask(private var carDao: CarDao?) : AsyncTask<Int, Unit, Unit>() {
-            override fun doInBackground(vararg params: Int?) {
-                carDao?.deleteById(params[0])
-            }
+    class DeleteCar(private var carDao: CarDao?, private var data: Int) : Runnable {
+        override fun run() {
+            carDao?.deleteById(data)
         }
+    }
 
-        class GetList(private var modelDao: ModelDao?) : AsyncTask<String?, Unit, ModelEntity>() {
-            override fun doInBackground(vararg params: String?): ModelEntity? {
-                return modelDao?.getStringManData(params[0])
-            }
+    class GetModelsList(private var modelDao: ModelDao?, private var manName: String?) :
+        Callable<ModelEntity> {
+        override fun call(): ModelEntity? {
+            return modelDao?.getStringManData(manName)
         }
     }
 }
